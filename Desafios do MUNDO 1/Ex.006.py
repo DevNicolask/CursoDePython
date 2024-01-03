5930863e48cc5f6fb8d6b1067df89fce8d6ba050
NicolaskS2 <151470479+NicolaskS2@users.noreply.github.com>
2024-01-03 16:33:51 -0300

    Alterações

--- Desafios do MUNDO 1/Ex.006.py
+++
@@ -1,7 +0,0 @@
-#versão Normal.
-n = int(input('Digite um número '))
-print(f'O dobro de {n} é {n*2}.\nO triplo de {n} é {n*3}.\nA raiz quadrada de {n} é {n**(1/2)}.')
-
-#Versão colorida.
-"""n = int(input('\033[1;35m Digite um número \033[m'))
-print(f' \033[1;98mO dobro de {n} é {n*2}.\033[m\n \033[1;93mO triplo de {n} é {n*3}.\033[m\n \033[1;91mA raiz quadrada de {n} é {n**(1/2)}. \033[m')"""
