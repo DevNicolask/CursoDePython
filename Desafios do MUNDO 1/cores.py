2ae953b8d35ce7666a479f015a5696c41b5fc580
NicolaskS2 <151470479+NicolaskS2@users.noreply.github.com>
2024-01-03 16:30:37 -0300

    Biblioteca de Cores
    Adicionei uma biblioteca de cores

---
+++ Desafios do MUNDO 1/cores.py
@@ -0,0 +1,17 @@
+class Cores:
+    preto = '\033[30m'
+    vermelho = '\033[31m'
+    verde = '\033[32m'
+    amarelo = '\033[33m'
+    laranja = '\033[34m'
+    roxo = '\033[35m'
+    ciano = '\033[36m'
+    branco = '\033[37m'
+    reset = '\033[0m'
+# Outra forma
+    vermelho2 = '\033[91m'
+    verde2 = '\033[92m'
+    amarelo = '\033[93m'
+    azul2 = '\033[94m'
+    magenta2 = '\033[95m'
+    ciano2 = '\033[96m'
