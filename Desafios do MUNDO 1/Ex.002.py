81d5a0eec83bd4898937abce709daa799c39180c
NicolaskS2 <151470479+NicolaskS2@users.noreply.github.com>
2024-01-03 16:27:58 -0300

    M1 D2

---
+++ Desafios do MUNDO 1/Ex.002.py
@@ -0,0 +1,9 @@
+# Código normal.
+nome = input('Qual é o seu nome? ')
+print('Prazer em te conhecer', nome)
+
+# Apendi como faz uma biblioteca em Python resolvi usar aqui!
+"""from cores import Cores
+
+nome = input(Cores.ciano2 + 'Qual é o seu nome? ' + Cores.reset)
+print(Cores.ciano2 + 'Prazer em te conhecer' + Cores.reset, nome)"""
