cb6e2f92c62e22212d9167e9f8777b9875b9a977
NicolaskS2 <151470479+NicolaskS2@users.noreply.github.com>
2024-01-04 19:40:38 -0300

    M1 D008

---
+++ Desafios M1/Ex.009.py
@@ -0,0 +1,28 @@
+# CÓDIGO NORMAL
+a = float(input('Me diga o número o qual você quer a tabuada '))
+print('-'*15)
+print(f'{a} *  2 = {a*2}.\n'
+      f'{a} *  3 = {a*3}.\n'
+      f'{a} *  4 = {a*4}.\n'
+      f'{a} *  5 = {a*5}.\n'
+      f'{a} *  6 = {a*6}.\n'
+      f'{a} *  7 = {a*7}.\n'
+      f'{a} *  8 = {a*8}.\n'
+      f'{a} *  9 = {a*9}.\n'
+      f'{a} * 10 = {a*10}.')
+print('-'*15)
+# CÓDIGO COLORIDO.
+"""a = float(input('\033[1;95mMe diga o número o qual você quer a tabuada '))
+print('\033[1;95m-\033[m'*15)
+print(f'\033[1;30m{a} *  1 = {a}.\n'
+      f'\033[1;31m{a} *  2 = {a*2}.\n'
+      f'\033[1;32m{a} *  3 = {a*3}.\n'
+      f'\033[1;33m{a} *  4 = {a*4}.\n'
+      f'\033[1;34m{a} *  5 = {a*5}.\n'
+      f'\033[1;35m{a} *  6 = {a*6}.\n'
+      f'\033[1;36m{a} *  7 = {a*7}.\n'
+      f'\033[1;37m{a} *  8 = {a*8}.\n'
+      f'\033[1;38m{a} *  9 = {a*9}.\n'
+      f'\033[5;30m{a} * 10 = {a*10}.\033[m')
+      print('\033[1;35m-\033[m'*15)"""
+
